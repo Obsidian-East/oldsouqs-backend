fa8ecf6ef5b53a9b284e73cc85a192ee6c0ad312
REXACKER <dibehjad02@gmail.com>
2025-03-09 15:44:45 +0200

    struct fix

--- models/user.go
+++ models/user.go
@@ -7,6 +7,7 @@ type User struct {
 	FirstName   string             `bson:"first_name"`
 	LastName    string             `bson:"last_name"`
 	PhoneNumber string             `bson:"phone_number"`
+	Location 	string			   `bson:"location"`
 	Email       string             `bson:"email"`
 	Password    string             `bson:"password"`
 }
