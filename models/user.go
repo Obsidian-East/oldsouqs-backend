daba9c5cb68741dd3fa97e88e2ca3f25f9953985
REXACKER <dibehjad02@gmail.com>
2025-05-07 20:57:48 +0300

    phone number

--- models/user.go
+++ models/user.go
@@ -6,10 +6,10 @@ type User struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	FirstName   string             `bson:"first_name" json:"first_name"`
 	LastName    string             `bson:"last_name" json:"last_name"`
-	PhoneNumber string             `bson:"phone_number" json:"phone_number"`
+	PhoneNumber string             `bson:"phonenumber" json:"phonenumber"`
 	Location    string             `bson:"location" json:"location"`
 	Email       string             `bson:"email" json:"email"`
-	Password    string             `bson:"password" json:"password"`
+	Password    string             `bson:"password" json:"-"`
   }
   
 
