6ef673ef94fa0f9b796b3f6a44b304454dcc1f40
REXACKER <dibehjad02@gmail.com>
2025-07-18 18:33:21 +0300

    collection

--- models/product.go
+++ models/product.go
@@ -7,26 +7,26 @@ import (
 )
 
 type Product struct {
-	ID 				primitive.ObjectID    `bson:"_id,omitempty" json:"id"`
-	Sku 			string				  `bson:"sku" json:"sku"`
-	Title 			string 				  `bson:"title" json:"title"`
-	TitleAr 		string 			      `bson:"titleAr" json:"titleAr"`
-	Description 	string                `bson:"description" json:"description"`
-	DescriptionAr   string                `bson:"descriptionAr" json:"descriptionAr"`
-	Price 			float64               `bson:"price" json:"price"`
-	Image 			string                `bson:"image" json:"image"`
-	Tag 			[]string              `bson:"tag" json:"tag"`
-	Stock 			int32                 `bson:"stock" json:"stock"`
-	CreatedAt 		time.Time             `bson:"createdAt" json:"createdAt"`
-	UpdatedAt 		time.Time             `bson:"updatedAt" json:"updatedAt"`
+	ID            primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Sku           string             `bson:"sku" json:"sku"`
+	Title         string             `bson:"title" json:"title"`
+	TitleAr       string             `bson:"titleAr" json:"titleAr"`
+	Description   string             `bson:"description" json:"description"`
+	DescriptionAr string             `bson:"descriptionAr" json:"descriptionAr"`
+	Price         float64            `bson:"price" json:"price"`
+	Image         string             `bson:"image" json:"image"`
+	Tag           []string           `bson:"tag" json:"tag"`
+	Stock         int32              `bson:"stock" json:"stock"`
+	CreatedAt     time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt     time.Time          `bson:"updatedAt" json:"updatedAt"`
 }
 
 type Collection struct {
-	ID               primitive.ObjectID   `bson:"_id,omitempty"`
-	CollectionName   string               `bson:"collectionName"`
-	CollectionNameAr string               `bson:"collectionNameAr"`
-	Description      string               `bson:"description"`
-	DescriptionAr    string               `bson:"descriptionAr"`
-	ProductIds       []primitive.ObjectID `bson:"productIds"`
-	ShowCollection   bool                 `bson:"showCollection"`
+	ID               primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
+	CollectionName   string               `bson:"collectionName" json:"collectionName"`
+	CollectionNameAr string               `bson:"collectionNameAr" json:"collectionNameAr"`
+	Description      string               `bson:"description" json:"description"`
+	DescriptionAr    string               `bson:"descriptionAr" json:"descriptionAr"`
+	ProductIds       []primitive.ObjectID `bson:"productIds" json:"productIds"`
+	ShowCollection   bool                 `bson:"showCollection" json:"showCollection"`
 }
